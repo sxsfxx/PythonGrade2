99bd086c089d4cfd36f810deb1d45a2879bad99f
luj <sxsfxx@gmail.com>
2022-04-21 12:46:00 +0800

    create a windmill by Manim.

---
+++ Manim/windmill.py
@@ -0,0 +1,35 @@
+"""
+manim -pql windmill.py Windmill
+"""
+
+from manim import *
+import math
+
+class Windmill(Scene):
+    def construct(self):
+        ax = Axes(x_length=10, y_length=10)
+        self.add(ax)
+
+        windmill = Group()
+        center = windmill.get_center()
+
+        whee = Circle(radius=3.1, arc_center=center)
+        whee.set_fill(PINK, opacity=0.5)
+        windmill.add(whee)
+        
+        for i in range(3):
+            angle = i*2*PI/3
+            bar = Line((0, 0, 0), (math.cos(angle+PI/6)*3, math.sin(angle+PI/6)*3, 0))
+            bar.set_fill(BLACK)
+            windmill.add(bar)
+            leaf = Sector(arc_center=center, outer_radius=3, inner_radius=0.5, angle=PI/6, start_angle=angle+PI/12)
+            leaf.set_fill(YELLOW, opacity=1.0)
+            windmill.add(leaf)
+
+        dot = Dot()
+        dot.set_fill(BLACK)
+        windmill.add(dot)
+
+        self.play(Rotate(windmill), run_time=5, rate_func=rush_into)
+        for i in range(10):
+            self.play(Rotate(windmill), run_time=1, rate_func=linear)
